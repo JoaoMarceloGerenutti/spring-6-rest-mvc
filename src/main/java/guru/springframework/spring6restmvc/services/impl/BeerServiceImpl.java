080631c05e1efd1512bffd623dd4623cc6347e05
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-12 17:48:15 -0300

    Adding builder example

---
+++ src/main/java/guru/springframework/spring6restmvc/services/impl/BeerServiceImpl.java
@@ -0,0 +1,29 @@
+package guru.springframework.spring6restmvc.services.impl;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import guru.springframework.spring6restmvc.services.BeerService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    @Override
+    public Beer getBerrById(UUID id) {
+        return Beer.builder()
+                .id(id)
+                .version(1)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("123456")
+                .price(new BigDecimal("12.99"))
+                .quantityOnHand(122)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+    }
+}
