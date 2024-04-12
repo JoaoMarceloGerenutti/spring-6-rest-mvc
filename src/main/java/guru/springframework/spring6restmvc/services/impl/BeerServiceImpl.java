68469725c971c5bf925fd4cb722feef826408900
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-12 18:38:28 -0300

    Adding lombok logging example

--- src/main/java/guru/springframework/spring6restmvc/services/impl/BeerServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/impl/BeerServiceImpl.java
@@ -3,17 +3,22 @@ package guru.springframework.spring6restmvc.services.impl;
 import guru.springframework.spring6restmvc.models.Beer;
 import guru.springframework.spring6restmvc.models.BeerStyle;
 import guru.springframework.spring6restmvc.services.BeerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
 
     @Override
     public Beer getBerrById(UUID id) {
+
+        log.debug("Get Beer by Id - in service. Id: " + id.toString());
+
         return Beer.builder()
                 .id(id)
                 .version(1)
