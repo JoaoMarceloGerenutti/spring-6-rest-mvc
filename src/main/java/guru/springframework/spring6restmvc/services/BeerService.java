dc30ada819eefe03bb6db7627f2df6f75ddd24d3
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-04-13 19:18:20 -0300

    Adding list beer endpoint

--- src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.models.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
 
     Beer getBerrById(UUID id);
+
+    List<Beer> listBeers();
 }
