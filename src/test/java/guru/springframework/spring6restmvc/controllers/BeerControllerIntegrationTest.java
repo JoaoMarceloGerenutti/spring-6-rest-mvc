03eee48c2e6a27901c9ee21a183332f69b7bb4dc
Joao Marcelo <joaomarcelogerenuttii@hotmail.com>
2024-05-06 15:06:47 -0300

    Adding integration test

--- src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIntegrationTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controllers/BeerControllerIntegrationTest.java
@@ -28,6 +28,7 @@ import java.util.UUID;
 import static org.hamcrest.core.Is.is;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -55,6 +56,15 @@ class BeerControllerIntegrationTest {
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void testListBeerByName() throws Exception {
+        mockMvc.perform(get(BeerController.BEER_PATH)
+                .queryParam("beerName", "IPA"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size", is(100)));
+
     }
 
     @Test
